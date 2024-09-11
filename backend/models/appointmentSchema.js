9f7a1883c773449ef73072886ce397ad6538d606
Shivyansh05 <128629467+Shivyansh05@users.noreply.github.com>
2024-09-12 02:03:03 +0530

    Update appointmentSchema.js

--- backend/models/appointmentSchema.js
+++ backend/models/appointmentSchema.js
@@ -21,8 +21,8 @@ const appointmentSchema = new mongoose.Schema({
   phone: {
     type: String,
     required: [true, "Phone Is Required!"],
-    minLength: [11, "Phone Number Must Contain Exact 11 Digits!"],
-    maxLength: [11, "Phone Number Must Contain Exact 11 Digits!"],
+    minLength: [10, "Phone Number Must Contain Exact 10 Digits!"],
+    maxLength: [10, "Phone Number Must Contain Exact 10 Digits!"],
   },
   nic: {
     type: String,
